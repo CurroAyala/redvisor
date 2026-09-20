2d24f07ea7aa04e7adeaa7c1819a323c31f149cf
CurroAyala <curroayala18@gmail.com>
2026-09-20 19:17:13 +0200

    chore: logo añadido a la ventana de la app

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Ejecutar desde la raíz del proyecto (donde vive el paquete `src`).
 
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from src.gui.main_window import VentanaPrincipal
 from src.gui.estilo import HOJA_DE_ESTILOS
 
@@ -12,6 +13,7 @@ from src.gui.estilo import HOJA_DE_ESTILOS
 def main():
     app = QApplication(sys.argv)
     app.setStyleSheet(HOJA_DE_ESTILOS)
+    app.setWindowIcon(QIcon("assets/logo_2.png"))
 
     ventana = VentanaPrincipal()
     ventana.show()
